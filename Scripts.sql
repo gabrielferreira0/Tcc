207902be924a1278eb9b1d8bb26ea662fa71dfa2
Gabriel Ferreira <47874487+gabrielferreira0@users.noreply.github.com>
2021-04-16 19:09:58 -0300

    add table typeUser

--- Scripts.sql
+++ Scripts.sql
@@ -1,4 +1,10 @@
 drop table usuarios;
+drop table tipoUsuario;
+
+create table tipoUsuario(
+    id serial primary key not null ,
+    perfil varchar (50)
+);
 
 create table usuarios(
                       id serial primary key not null,
@@ -8,7 +14,12 @@ create table usuarios(
                       usucpf varchar (50),
                       usutelefone varchar (50),
                       usufoto varchar (100),
-                      usustatus varchar (20)
+                      usustatus varchar (20),
+                      usutipo int REFERENCES tipoUsuario (id)
 );
 
+INSERT INTO tipoUsuario (perfil) VALUES ('Admin');
+INSERT INTO tipoUsuario (perfil) VALUES ('Cliente');
+INSERT INTO tipoUsuario (perfil) VALUES ('Profissional');
 select * from usuarios;
+select * from tipoUsuario;
