dabed8bcdd27816536b85e4528f60aa063d9a1d1
Gabriel Ferreira <47874487+gabrielferreira0@users.noreply.github.com>
2021-08-09 20:40:42 -0300

    ajustes db host

--- src/Model/DBconexao.php
+++ src/Model/DBconexao.php
@@ -1,11 +1,18 @@
 <?php
 
 Class DBconexao {
-    protected $host = "localhost";
+//    protected $host = "localhost";
+//    protected $port = "5432";
+//    protected $dbname = "tcc";
+//    protected $user = "postgres";
+//    protected $password = "admin";
+//    protected $db;
+
+    protected $host = "51.15.164.106";
     protected $port = "5432";
-    protected $dbname = "tcc";
-    protected $user = "postgres";
-    protected $password = "admin";
+    protected $dbname = "aqbarhch_wedoDB";
+    protected $user = "aqbarhch_wedoADM";
+    protected $password = "aKQm~F;j)i_G";
     protected $db;
 
     function __construct(){
