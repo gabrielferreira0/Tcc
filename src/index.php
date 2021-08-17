44be8ee122e0fc46044e7aabce1058d8907fa1a2
Gabriel Ferreira <47874487+gabrielferreira0@users.noreply.github.com>
2021-08-17 17:07:27 -0300

    ajustando meta

--- src/index.php
+++ src/index.php
@@ -7,7 +7,7 @@ session_start();
     <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Wedo Serviços">
-    <meta property="og:image" content="imagens/logo.png">
+    <meta property="og:image" content="https://www.wedosvc.com.br/src/imagens/logo.png">
     <title>Serviços</title>
     <link href="View/docs/fonts/fontawesome/5.12.1/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="View/docs/plugins/bootstrap-4.1.3-dist/css/bootstrap.min.css">
