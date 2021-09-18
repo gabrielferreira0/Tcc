1ac1baac4caf53a2a14c4c8cee1d12aaac9a355c
Gabriel Ferreira <47874487+gabrielferreira0@users.noreply.github.com>
2021-09-18 00:37:50 -0300

    ajustes

--- src/View/pedidoServico.php
+++ src/View/pedidoServico.php
@@ -27,13 +27,12 @@ include('Navbar.php');
 
 
         $('#Conteudo').on('click', '.expandir', function () {
-             let classe = $(this).children().attr('class');
-             if (classe=='fas fa-caret-square-down'){
-                 $(this).children().removeClass().addClass('fas fa-caret-square-up')
-             }
-             else {
-                 $(this).children().removeClass().addClass('fas fa-caret-square-down')
-             }
+            let classe = $(this).children().attr('class');
+            if (classe == 'fas fa-caret-square-down') {
+                $(this).children().removeClass().addClass('fas fa-caret-square-up')
+            } else {
+                $(this).children().removeClass().addClass('fas fa-caret-square-down')
+            }
         });
 
     });
@@ -47,7 +46,7 @@ include('Navbar.php');
             Finalizar Pedido <i class="fas fa-money-check"></i>
         </h1>
 
-        <div class="container row">
+         <div style="margin: 0" class="container row">
             <div class="col-md-8 col-12 pl-5 pr-5 infoProfissional">
                 <h3>Pintor / Casa ou apartamento</h3>
                 <span>Nome: Lucca Edson Calebe </span> <br>
@@ -65,7 +64,7 @@ include('Navbar.php');
                 </div>
 
                 <div class="col-md-12 d-flex justify-content-center">
-                    <img  style="cursor:default;" class ="miniatura" src="../imagens/usuarios/' . $_SESSION['Foto'] . '">
+                    <img alt ="avatar-profissional" style="cursor:default;" class ="miniatura" src="../imagens/usuarios/' . $_SESSION['Foto'] . '">
                 </div>
                  </div>';
             } else {
@@ -91,13 +90,14 @@ include('Navbar.php');
             <form id="formulario" class="formulario" data-toggle="validator" enctype="multipart/form-data">
                 <h1>Pagamento
 
-                    <a title="expandir" class="btn btn-secondary expandir" data-toggle="collapse" href="#pagamento" role="button" aria-expanded="false">
+                    <a title="expandir" class="btn btn-secondary expandir" data-toggle="collapse" href="#pagamento"
+                       role="button" aria-expanded="false">
                         <i style="cursor: pointer" class="fas fa-caret-square-down"></i>
                     </a>
                 </h1>
+                <p class="small texto-cinza">Forneça as informações de pagamento</p>
 
-
-                <div id="pagamento"  class="collapse">
+                <div id="pagamento" class="collapse">
                     <div class="form-row texto-cinza">
                         <div class="form-group col-md-3">
                             <label for="Username">numero do cartão de crédito:</label>
@@ -116,7 +116,7 @@ include('Navbar.php');
                         </div>
                         <div class="form-group col-md-3">
                             <label for="Username">Nome impresso no cartão:</label>
-                            <div class="input-group input-group-sm ">
+                            <div class="input-group input-group-sm">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text arredondar"><i class="far fa-credit-card"></i></span>
                                 </div>
@@ -146,7 +146,8 @@ include('Navbar.php');
                                 <div class="input-group-prepend">
                                     <span class="input-group-text arredondar"><i class="fas fa-key"></i></span>
                                 </div>
-                                <input maxlength="3" type="password" class="form-control arredondar" placeholder="Código"
+                                <input maxlength="3" type="password" class="form-control arredondar"
+                                       placeholder="Código"
                                        required>
                             </div>
                             <div class="error help-block with-errors"></div>
@@ -155,21 +156,120 @@ include('Navbar.php');
                 </div>
 
                 <h1>Endereço
-                    <a class="btn btn-secondary expandir" data-toggle="collapse" href="#endereco" role="button" aria-expanded="false" aria-controls="collapseExample">
+                    <a class="btn btn-secondary expandir" data-toggle="collapse" href="#endereco" role="button"
+                       aria-expanded="false" aria-controls="collapseExample">
                         <i style="cursor: pointer" class="fas fa-caret-square-down"></i>
                     </a>
                 </h1>
-                <label class="small texto-cinza">Forneça o endereço para a realização do serviço</label>
+                <p class="small texto-cinza">Forneça o endereço para a realização do serviço</p>
 
-                <div id="endereco"  class="collapse">
-                <h1>Dados do endereço aqui</h1>
+                <div id="endereco" class="collapse">
+                    <div class="form-row texto-cinza">
+                        <div class="form-group col-md-2 texto-cinza">
+                            <label for="CEP">CEP:</label>
+
+                            <div class="input-group input-group-sm">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text arredondar"> <i class="fas fa-home"></i></span>
+                                </div>
+                                <label for="CEP"></label>
+                                <input type="text" class="form-control arredondar"  id="CEP"
+                                       placeholder="12.123-123" data-error="Por favor, informe um CEP correto."
+                                       required="">
+                            </div>
+
+                            <div class="error help-block with-errors"></div>
+                        </div>
+                        <div class="form-group col-md-2 texto-cinza">
+                            <label for="bairro">Bairro:</label>
+
+                            <div class="input-group input-group-sm">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text arredondar"> <i class="fas fa-home"></i></span>
+                                </div>
+                                <input type="text" class="form-control arredondar" id=bairro"
+                                       data-error="Por favor, informe um bairro correto." required="">
+                            </div>
+
+                            <div class="error help-block with-errors"></div>
+                        </div>
+                        <div class="form-group col-md-2 texto-cinza">
+                            <label for="logradouro">Logradouro:</label>
+
+                            <div class="input-group input-group-sm">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text arredondar"> <i class="fas fa-home"></i></span>
+                                </div>
+                                <input type="text" class="form-control arredondar" id="logradouro"
+                                       data-error="Por favor, informe um Logradouro correto." required="">
+                            </div>
+
+                            <div class="error help-block with-errors"></div>
+                        </div>
+                    </div>
+
+                    <div class="form-row texto-cinza">
+                        <div class="form-group col-md-2 texto-cinza">
+                            <label for="complemento">Complemento:</label>
+
+                            <div class="input-group input-group-sm">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text arredondar"> <i class="fas fa-home"></i></span>
+                                </div>
+                                <label for="CEP"></label>
+                                <input type="text" class="form-control arredondar" id="complemento"
+                                       data-error="Por favor, informe um Logradouro correto." required="">
+                            </div>
+
+                            <div class="error help-block with-errors"></div>
+                        </div>
+                        <div class="form-group col-md-2 texto-cinza">
+                            <label for="numero">Numero:</label>
+
+                            <div class="input-group input-group-sm">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text arredondar"> <i class="fas fa-home"></i></span>
+                                </div>
+                                <label for="numero"></label>
+                                <input type="text" class="form-control arredondar" id="numero"
+                                       data-error="Por favor, informe um numero correto." required="">
+                            </div>
+
+                            <div class="error help-block with-errors"></div>
+                        </div>
+                        <div class="form-group col-md-2 texto-cinza">
+                            <label for="cidade">Cidade/UF</label>
+
+                            <div class="input-group input-group-sm">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text arredondar"> <i class="fas fa-home"></i></span>
+                                </div>
+                                <label for="numero"></label>
+                                <input value="Brasilia" type="text" class="form-control arredondar" id="cidade"
+                                       required disabled>
+
+                                <div class="input-group-append arredondar">
+                                    <span class="input-group-text">DF</span>
+                                </div>
+
+                            </div>
+
+
+                        </div>
+                    </div>
                 </div>
 
 
             </form>
         </div>
 
+        <div class="form-group" style="display:flex;  justify-content: center;">
+            <button id="" type="button" class="btn btn-danger ml-2 mr-2">Voltar</button>
+            <button id="" type="button" class="btn btn-success ml-2 mr-2">Finalizar Pedido</button>
+        </div>
+
     </div>
+
 </div>
 
 
