ed0effed8f868bde38a6361f452119faa90d36c0
Gabriel Ferreira <47874487+gabrielferreira0@users.noreply.github.com>
2021-09-28 15:02:54 -0300

    teste

--- src/index.php
+++ src/index.php
@@ -8,7 +8,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="SOLUÇÃO PARA INTERMEDIAR CONTRATAÇÃO DE SERVIÇOS DE PEQUENOS REPAROS">
     <meta property="og:image" content="https://getbootstrap.com/docs/4.5/assets/brand/bootstrap-social-logo.png">
-    <link rel="shortcut icon" type="image/x-icon" href="./imagens/logo.png">
+    <link rel="shortcut icon" type="image/x-icon" href="./imagens/thumbnails/tn_logo.png">
     <title>Serviços</title>
     <link href="View/docs/fonts/fontawesome/5.12.1/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="View/docs/plugins/bootstrap-4.1.3-dist/css/bootstrap.min.css">
